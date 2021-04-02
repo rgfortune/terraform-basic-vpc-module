34a27885311b877ef0468a99012fc9ffd767ad5f
Ricardo Fortune <ricardo.g.fortune@gmail.com>
2021-04-02 16:37:55 +0100

    Add VPC CIDR to outputs

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,7 @@
 #------------------------------------------- 
 
 output "vpc_id" { value = aws_vpc.vpc.id }
+output "vpc_cidr_block" { value = aws_vpc.vpc.cidr_block }
 
 output "env" { value = var.env }
 
