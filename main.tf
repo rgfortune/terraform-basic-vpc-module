29b058daa158a8b01f56b796883757058db77fbc
Ricardo Fortune <ricardo.g.fortune@gmail.com>
2022-02-14 17:44:07 -0400

    Update Internet Gateway resource name

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_vpc" "vpc" {
 # Internet Gateway
 #------------------------------------------- 
 
-resource "aws_internet_gateway" "gw" {
+resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
 }
 
@@ -34,7 +34,7 @@ resource "aws_route_table" "public_route_table" {
   vpc_id = aws_vpc.vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
+    gateway_id = aws_internet_gateway.igw.id
   }
   tags = { Name = "Public Route Table" }
 }
