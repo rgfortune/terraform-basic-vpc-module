e9ff8befd31ed4d150328083aa1b7c5e32ce85a8
Ricardo Fortune <ricardo.g.fortune@gmail.com>
2022-05-14 14:47:24 +0100

    Increase the size of prefix for subnetting VPC CIDR

--- locals.tf
+++ locals.tf
@@ -2,6 +2,6 @@ locals {
   availability_zones = slice(data.aws_availability_zones.available.names, 0, var.availability_zones_count)
 
   # Calculate subnet CIDR lists
-  public_subnet_list  = [for subnet in range(var.public_subnets_count) : cidrsubnet(var.cidr, 3, subnet)]
-  private_subnet_list = [for subnet in range(var.public_subnets_count, var.public_subnets_count + var.private_subnets_count) : cidrsubnet(var.cidr, 3, subnet)]
+  public_subnet_list  = [for subnet in range(var.public_subnets_count) : cidrsubnet(var.cidr, 6, subnet)]
+  private_subnet_list = [for subnet in range(var.public_subnets_count, var.public_subnets_count + var.private_subnets_count) : cidrsubnet(var.cidr, 6, subnet)]
 }
