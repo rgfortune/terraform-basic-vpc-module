f0f6b48cb431774156b7e49ebe6fa5e5dd801ce7
Ricardo Fortune <ricardo.g.fortune@gmail.com>
2022-05-15 12:22:20 +0100

    Export the number of availabilty zones in use

--- outputs.tf
+++ outputs.tf
@@ -5,5 +5,7 @@
 output "vpc_id" { value = aws_vpc.vpc.id }
 output "vpc_cidr_block" { value = aws_vpc.vpc.cidr_block }
 
+output "aws_availabilityZonesCount" { value = var.availability_zones_count }
+
 output "aws_publicSubnets" { value = aws_subnet.publicSubnets }
 output "aws_privateSubnets" { value = aws_subnet.privateSubnets }
