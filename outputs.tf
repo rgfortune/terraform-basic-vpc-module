95fe59bcd5001f4e792d26a7b06933174f1feb47
Ricardo Fortune <ricardo.g.fortune@gmail.com>
2023-08-03 16:04:19 +0100

    Add route tables to module output

--- outputs.tf
+++ outputs.tf
@@ -14,3 +14,6 @@ output "public_subnets" { value = aws_subnet.public_subnets }
 output "private_subnets_00" { value = aws_subnet.private_subnets_00 }
 output "private_subnets_01" { value = aws_subnet.private_subnets_01 }
 output "private_subnets_02" { value = aws_subnet.private_subnets_02 }
+
+output "private_route_tables" { value = aws_route_table.private_route_tables}
+output "public_route_table" { value = aws_route_table.public_route_table}
